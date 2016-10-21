04b9d7920c2861916f1412b5b420d68048504870
Merlyn Morgan-Graham <kavika@gmail.com>
2016-10-21 00:36:50 -0700

    Make child scene objects invisible if parent is invisible

--- src/engine/scene.cpp
+++ src/engine/scene.cpp
@@ -42,13 +42,16 @@ void Scene::Draw(double time_since_last_frame) {
       previous_material = mesh_component->material.get();
     }
 
-    matrix_stack.push(current_matrix);
+    // TODO: Should is_visible be on a scene object instead?
+    if (!mesh_component || (mesh_component && mesh_component->is_visible)) {
+      matrix_stack.push(current_matrix);
 
-    for (auto& child_scene_object: scene_object->children) {
-      render_scene_object(child_scene_object);
+      for (auto& child_scene_object: scene_object->children) {
+        render_scene_object(child_scene_object);
+      }
+
+      matrix_stack.pop();
     }
-
-    matrix_stack.pop();
   };
 
   for (auto& scene_object: objects) {
