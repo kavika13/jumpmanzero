893d42df00e4c15ee274dbb2aaa3a7a8e1b03987
Merlyn Morgan-Graham <kavika@gmail.com>
2019-10-09 00:34:43 -0700

    Extract level data from level binaries and export to Lua scripts

---
+++ JumpmanAssets/Data/level_options.lua
@@ -0,0 +1,42 @@
+return {
+    music_background_track_filename = "sound/menu.mid",
+    music_death_track_filename = "sound/death.mid",
+    music_win_track_filename = "sound/endlevel.mid",
+    music_loop_start_music_time = 0,
+    meshes = {
+    },
+    textures = {
+        { name = "MENUBACK", filename = "data/menuback.jpg" },
+        { name = "BORINGBLUE", filename = "data/boringblue.bmp" },
+        { name = "RACHBLUE", filename = "data/rachblue.bmp" },
+    },
+    sounds = {
+        { name = "SELECT", filename = "sound/select.wav" },
+        { name = "FIRE", filename = "sound/fire.wav" },
+    },
+    donuts = {
+    },
+    platforms = {
+    },
+    ladders = {
+    },
+    vines = {
+    },
+    walls = {
+    },
+    backdrops = {
+        {
+            number = 100,
+            texture_index = 0,
+            pos = { 80, 40 },
+            mesh = {
+                { pos = { -70, 220, 80 }, normal = { 0, 0, -1 }, uv = { 0, 0 } },
+                { pos = { 230, 220, 80 }, normal = { 0, 0, -1 }, uv = { 1, 0 } },
+                { pos = { -70, -140, 80 }, normal = { 0, 0, -1 }, uv = { 0, 1 } },
+                { pos = { 230, 220, 80 }, normal = { 0, 0, -1 }, uv = { 1, 0 } },
+                { pos = { 230, -140, 80 }, normal = { 0, 0, -1 }, uv = { 1, 1 } },
+                { pos = { -70, -140, 80 }, normal = { 0, 0, -1 }, uv = { 0, 1 } },
+            },
+        },
+    },
+};
