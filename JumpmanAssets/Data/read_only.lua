c404c5c5b428ab6cd78af72f8074d7ac79b20a1f
Merlyn Morgan-Graham <kavika@gmail.com>
2019-10-09 01:48:00 -0700

    Add iteration support to tables returned by make_table_read_only

--- JumpmanAssets/Data/read_only.lua
+++ JumpmanAssets/Data/read_only.lua
@@ -4,9 +4,11 @@ function Module.make_table_read_only(tbl)
     return setmetatable({}, {
         __index = tbl,
         __newindex = function(t, key, value)
-            error("attempting to change constant " ..
-                   tostring(key) .. " to " .. tostring(value), 2)
-        end
+            error("attempting to change constant " .. tostring(key) .. " to " .. tostring(value), 2);
+        end,
+        __pairs = function(t)
+            return next, tbl, nil;
+        end,
     });
 end
 
