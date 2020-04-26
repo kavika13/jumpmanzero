98d76ea0969f1aeab2039180c831ab8b5e7f32aa
Merlyn Morgan-Graham <kavika@gmail.com>
2020-04-26 15:50:02 -0700

    Cleanup magic number constants in shark (level25) animation code

--- JumpmanAssets/data/shark.lua
+++ JumpmanAssets/data/shark.lua
@@ -1,3 +1,5 @@
+local read_only = require "data/read_only";
+
 local Module = {};
 
 Module.GameLogic = nil;
@@ -14,6 +16,27 @@ Module.CurrentPosY = 0;
 Module.CurrentPosZ = 0;
 Module.CurrentVelocityX = 0;
 Module.CurrentVelocityY = 0;
+
+local animation_frame = {
+    SWIM_RIGHT_1 = 1,
+    SWIM_RIGHT_2 = 2,
+    SWIM_RIGHT_3 = 3,
+    SWIM_RIGHT_4 = 4,
+
+    TURN_RIGHT_1 = 5,
+    TURN_RIGHT_2 = 6,
+    TURN_RIGHT_3 = 7,
+
+    SWIM_LEFT_1 = 11,
+    SWIM_LEFT_2 = 12,
+    SWIM_LEFT_3 = 13,
+    SWIM_LEFT_4 = 14,
+
+    TURN_LEFT_1 = 15,
+    TURN_LEFT_2 = 16,
+    TURN_LEFT_3 = 17,
+};
+animation_frame = read_only.make_table_read_only(animation_frame);
 
 local g_shark_mesh = nil;
 local g_animation_mesh_indices = {};
@@ -45,18 +68,18 @@ local function MoveShark_(game_input)
         Module.CurrentVelocityY = Module.CurrentVelocityY + 0.09;
     end
 
-    local is_player_visible = false;  -- TODO: Is this the right variable name?
+    local shark_can_see_player = false;
 
     if player_current_pos_x < Module.CurrentPosX - 2 and Module.CurrentVelocityX < 0 then
-        is_player_visible = true;
+        shark_can_see_player = true;
     end
 
     if player_current_pos_x > Module.CurrentPosX + 2 and Module.CurrentVelocityX > 0 then
-        is_player_visible = true;
+        shark_can_see_player = true;
     end
 
     if PlayerMoving_(game_input) == 0 or player_current_pos_y > 113 then
-        is_player_visible = false;
+        shark_can_see_player = false;
     end
 
     local iYSpeed;
@@ -67,7 +90,7 @@ local function MoveShark_(game_input)
         iYSpeed = (0 - Module.CurrentVelocityX) * Module.CurrentVelocityY;
     end
 
-    if is_player_visible then
+    if shark_can_see_player then
         Module.CurrentPosY = Module.CurrentPosY + iYSpeed;
     elseif player_current_pos_y < Module.CurrentPosY - 20 or player_current_pos_y > Module.CurrentPosY + 20 or player_current_pos_y > 114 then
         Module.CurrentPosY = Module.CurrentPosY + iYSpeed / 3;
@@ -86,11 +109,11 @@ local function MoveShark_(game_input)
 
         if Module.CurrentVelocityX < 0 then
             if g_turn_animation_frame_counter < 5 then
-                g_current_animation_frame_index = 15;  -- TODO: Don't hard-code animation frame indices
+                g_current_animation_frame_index = animation_frame.TURN_LEFT_1;
             elseif g_turn_animation_frame_counter < 10 then
-                g_current_animation_frame_index = 16;  -- TODO: Don't hard-code animation frame indices
+                g_current_animation_frame_index = animation_frame.TURN_LEFT_2;
             elseif g_turn_animation_frame_counter < 15 then
-                g_current_animation_frame_index = 17;  -- TODO: Don't hard-code animation frame indices
+                g_current_animation_frame_index = animation_frame.TURN_LEFT_3;
             else
                 g_turn_animation_frame_counter = 0;
                 g_turn_animation_is_active = false;
@@ -98,11 +121,11 @@ local function MoveShark_(game_input)
             end
         else
             if g_turn_animation_frame_counter < 5 then
-                g_current_animation_frame_index = 5;  -- TODO: Don't hard-code animation frame indices
+                g_current_animation_frame_index = animation_frame.TURN_RIGHT_1;
             elseif g_turn_animation_frame_counter < 10 then
-                g_current_animation_frame_index = 6;  -- TODO: Don't hard-code animation frame indices
+                g_current_animation_frame_index = animation_frame.TURN_RIGHT_2;
             elseif g_turn_animation_frame_counter < 15 then
-                g_current_animation_frame_index = 7;  -- TODO: Don't hard-code animation frame indices
+                g_current_animation_frame_index = animation_frame.TURN_RIGHT_3;
             else
                 g_turn_animation_frame_counter = 0;
                 g_turn_animation_is_active = false;
@@ -142,33 +165,32 @@ local function MoveShark_(game_input)
     end
 
     if Module.CurrentVelocityX > 0 then
-        g_current_animation_frame_index = 1 + g_move_animation_frame_index;  -- TODO: Don't hard-code animation frame indices
+        g_current_animation_frame_index = animation_frame.SWIM_RIGHT_1 + g_move_animation_frame_index;
     else
-        g_current_animation_frame_index = 11 + g_move_animation_frame_index;  -- TODO: Don't hard-code animation frame indices
+        g_current_animation_frame_index = animation_frame.SWIM_LEFT_1 + g_move_animation_frame_index;
     end
 end
 
 function Module.initialize()
-    -- TODO: Don't hard-code animation frame indices
-    g_animation_mesh_indices[1] = Module.MoveRightMeshResourceIndices[1];
-    g_animation_mesh_indices[2] = Module.MoveRightMeshResourceIndices[2];
-    g_animation_mesh_indices[3] = Module.MoveRightMeshResourceIndices[3];
-    g_animation_mesh_indices[4] = Module.MoveRightMeshResourceIndices[4];
+    g_animation_mesh_indices[animation_frame.SWIM_RIGHT_1] = Module.MoveRightMeshResourceIndices[1];
+    g_animation_mesh_indices[animation_frame.SWIM_RIGHT_2] = Module.MoveRightMeshResourceIndices[2];
+    g_animation_mesh_indices[animation_frame.SWIM_RIGHT_3] = Module.MoveRightMeshResourceIndices[3];
+    g_animation_mesh_indices[animation_frame.SWIM_RIGHT_4] = Module.MoveRightMeshResourceIndices[4];
 
-    g_animation_mesh_indices[5] = Module.TurnRightMeshResourceIndices[1];
-    g_animation_mesh_indices[6] = Module.TurnRightMeshResourceIndices[2];
-    g_animation_mesh_indices[7] = Module.TurnRightMeshResourceIndices[3];
+    g_animation_mesh_indices[animation_frame.TURN_RIGHT_1] = Module.TurnRightMeshResourceIndices[1];
+    g_animation_mesh_indices[animation_frame.TURN_RIGHT_2] = Module.TurnRightMeshResourceIndices[2];
+    g_animation_mesh_indices[animation_frame.TURN_RIGHT_3] = Module.TurnRightMeshResourceIndices[3];
 
-    g_animation_mesh_indices[11] = Module.MoveLeftMeshResourceIndices[1];
-    g_animation_mesh_indices[12] = Module.MoveLeftMeshResourceIndices[2];
-    g_animation_mesh_indices[13] = Module.MoveLeftMeshResourceIndices[3];
-    g_animation_mesh_indices[14] = Module.MoveLeftMeshResourceIndices[4];
+    g_animation_mesh_indices[animation_frame.SWIM_LEFT_1] = Module.MoveLeftMeshResourceIndices[1];
+    g_animation_mesh_indices[animation_frame.SWIM_LEFT_2] = Module.MoveLeftMeshResourceIndices[2];
+    g_animation_mesh_indices[animation_frame.SWIM_LEFT_3] = Module.MoveLeftMeshResourceIndices[3];
+    g_animation_mesh_indices[animation_frame.SWIM_LEFT_4] = Module.MoveLeftMeshResourceIndices[4];
 
-    g_animation_mesh_indices[15] = Module.TurnLeftMeshResourceIndices[1];
-    g_animation_mesh_indices[16] = Module.TurnLeftMeshResourceIndices[2];
-    g_animation_mesh_indices[17] = Module.TurnLeftMeshResourceIndices[3];
+    g_animation_mesh_indices[animation_frame.TURN_LEFT_1] = Module.TurnLeftMeshResourceIndices[1];
+    g_animation_mesh_indices[animation_frame.TURN_LEFT_2] = Module.TurnLeftMeshResourceIndices[2];
+    g_animation_mesh_indices[animation_frame.TURN_LEFT_3] = Module.TurnLeftMeshResourceIndices[3];
 
-    g_shark_mesh = new_mesh(g_animation_mesh_indices[1]);  -- TODO: Don't hard-code animation frame indices
+    g_shark_mesh = new_mesh(g_animation_mesh_indices[animation_frame.SWIM_RIGHT_1]);
     move_mesh_to_front(g_shark_mesh);
     set_mesh_texture(g_shark_mesh, Module.TextureResourceIndex);
     set_mesh_is_visible(g_shark_mesh, true);
@@ -177,7 +199,7 @@ function Module.initialize()
     Module.CurrentPosY = Module.StartPosY;
     Module.CurrentPosZ = 0.05;
     Module.CurrentVelocityX = 1;
-    g_current_animation_frame_index = 1;  -- TODO: Don't hard-code animation frame indices
+    g_current_animation_frame_index = animation_frame.SWIM_RIGHT_1;
 end
 
 function Module.update(game_input)
