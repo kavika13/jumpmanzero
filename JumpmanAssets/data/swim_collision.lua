d517e8c41a33bd4c90b6a8659b9790e8c9be0a59
Merlyn Morgan-Graham <kavika@gmail.com>
2021-05-13 22:35:13 -0700

    Rename some variables in swim module to make them more clear

--- JumpmanAssets/data/swim_collision.lua
+++ JumpmanAssets/data/swim_collision.lua
@@ -28,16 +28,15 @@ Module.IsInTank = false;
 Module.SharkObject = nil;
 Module.FacingDirection = 0;  -- TODO: This doesn't get set to anything but 0 by the parent module. Is it the shark's facing direction or the players? Maybe just simplify the code?
 
-local function DoCollide_(iX1, iY1, iX2, iY2)
-    -- TODO: Find out what iX1, iY1, iX2, iY2 are, more specifically, and rename them
+local function DoCollide_(player_left_x, player_bottom_y, player_right_x, player_top_y)
     local donut_count = Module.GameLogic.get_donut_object_count();
     local is_win_detected = true;
 
     for donut_index = 0, donut_count - 1 do
         local current_donut = Module.GameLogic.get_donut(donut_index);
 
-        if iX1 < current_donut.pos[1] + 2 and iX2 > current_donut.pos[1] - 2 then
-            if iY1 < current_donut.pos[2] + 2 and iY2 > current_donut.pos[2] - 2 then
+        if player_left_x < current_donut.pos[1] + 2 and player_right_x > current_donut.pos[1] - 2 then
+            if player_bottom_y < current_donut.pos[2] + 2 and player_top_y > current_donut.pos[2] - 2 then
                 if not Module.GameLogic.get_donut_is_collected(donut_index) then
                     play_sound_effect(Module.ChompSoundIndex);
                 end
@@ -61,20 +60,20 @@ local function DoCollide_(iX1, iY1, iX2, iY2)
         return;
     end
 
-    local iDX = Module.SharkObject.CurrentPosX;
-    local iDY = Module.SharkObject.CurrentPosY;
+    local shark_pos_x = Module.SharkObject.CurrentPosX;
+    local shark_pos_y = Module.SharkObject.CurrentPosY;
 
     if Module.SharkObject.CurrentVelocityX > 0 then
-        if iX1 < iDX + 7 and iX2 > iDX + 1 then
-            if iY1 < iDY + 7 and iY2 > iDY + 1 then
+        if player_left_x < shark_pos_x + 7 and player_right_x > shark_pos_x + 1 then
+            if player_bottom_y < shark_pos_y + 7 and player_top_y > shark_pos_y + 1 then
                 -- TODO: Handle case where jumpman jumps out but gets crunched. Currently can still live
                 play_sound_effect(Module.CrunchSoundIndex);
                 Module.GameLogic.kill();
             end
         end
     else
-        if iX1 < iDX - 1 and iX2 > iDX - 7 then
-            if iY1 < iDY + 7 and iY2 > iDY + 1 then
+        if player_left_x < shark_pos_x - 1 and player_right_x > shark_pos_x - 7 then
+            if player_bottom_y < shark_pos_y + 7 and player_top_y > shark_pos_y + 1 then
                 -- TODO: Handle case where jumpman jumps out but gets crunched. Currently can still live
                 play_sound_effect(Module.CrunchSoundIndex);
                 Module.GameLogic.kill();
@@ -92,21 +91,21 @@ function Module.update()
         return;
     end
 
-    local iPX = Module.GameLogic.get_player_current_position_x();
-    local iPY = Module.GameLogic.get_player_current_position_y();
+    local player_pos_x = Module.GameLogic.get_player_current_position_x();
+    local player_pos_y = Module.GameLogic.get_player_current_position_y();
 
     if Module.FacingDirection == 0 then
-        DoCollide_(iPX - 4, iPY + 3, iPX + 4, iPY + 7);
+        DoCollide_(player_pos_x - 4, player_pos_y + 3, player_pos_x + 4, player_pos_y + 7);
     elseif Module.FacingDirection == 1 then
-        DoCollide_(iPX - 3, iPY, iPX + 3, iPY + 10);
+        DoCollide_(player_pos_x - 3, player_pos_y, player_pos_x + 3, player_pos_y + 10);
     elseif Module.FacingDirection == 2 then
-        DoCollide_(iPX - 8, iPY + 6, iPX - 2, iPY + 8);
-        DoCollide_(iPX + 2, iPY + 2, iPX + 8, iPY + 4);
+        DoCollide_(player_pos_x - 8, player_pos_y + 6, player_pos_x - 2, player_pos_y + 8);
+        DoCollide_(player_pos_x + 2, player_pos_y + 2, player_pos_x + 8, player_pos_y + 4);
     elseif Module.FacingDirection == 3 then
-        DoCollide_(iPX - 5, iPY + 4, iPX + 5, iPY + 6);
+        DoCollide_(player_pos_x - 5, player_pos_y + 4, player_pos_x + 5, player_pos_y + 6);
     elseif Module.FacingDirection == 4 then
-        DoCollide_(iPX + 2, iPY + 6, iPX + 8, iPY  +8);
-        DoCollide_(iPX - 8, iPY + 2, iPX - 2, iPY + 4);
+        DoCollide_(player_pos_x + 2, player_pos_y + 6, player_pos_x + 8, player_pos_y  +8);
+        DoCollide_(player_pos_x - 8, player_pos_y + 2, player_pos_x - 2, player_pos_y + 4);
     end
 end
 
