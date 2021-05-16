782265e3182b543a2589d6d6627503953ebb82a0
Merlyn Morgan-Graham <kavika@gmail.com>
2021-05-15 18:28:44 -0700

    Fix debug logging cutting off last character, switch to stbsp_vsnprintf

--- Jumpman/logging.c
+++ Jumpman/logging.c
@@ -1,7 +1,8 @@
 #include "logging.h"
 
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <stb_sprintf.h>
 
 #if defined(_WIN32)
     #include <malloc.h>
@@ -9,13 +10,12 @@
 #endif
 
 static int vdebug_log(char const* const format_string, va_list argp) {
-    // TODO: Use stb_sprintf lib?
 #if defined(_WIN32)
-    int len = vsnprintf(NULL, 0, format_string, argp);
+    int len = stbsp_vsnprintf(NULL, 0, format_string, argp) + 1;
     char* error_message_buffer = malloc(len);
 
     if(error_message_buffer) {
-        int outlen = vsnprintf(error_message_buffer, len, format_string, argp);
+        int outlen = stbsp_vsnprintf(error_message_buffer, len, format_string, argp) + 1;
 
         if(outlen == len) {
             OutputDebugStringA(error_message_buffer);
