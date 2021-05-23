ada7b64e6e284ad58d1b65bd06f462dec31695c0
Merlyn Morgan-Graham <kavika@gmail.com>
2021-05-22 17:29:18 -0700

    Refactor direct matrix manipulation to transforms in level7 (pause_wave)

--- JumpmanAssets/data/pause_wave.lua
+++ JumpmanAssets/data/pause_wave.lua
@@ -12,9 +12,12 @@ Module.Wave2TextureResourceIndex = 0;
 
 Module.TargetWaveHeight = 0;
 
-local g_wave_front_mesh_index;
-local g_wave_back_mesh_index;
-local g_sea_mesh_index;
+local g_wave_front_mesh_index = -1;
+local g_wave_front_transform_index = -1;
+local g_wave_back_mesh_index = -1;
+local g_wave_back_transform_index = -1;
+local g_sea_mesh_index = -1;
+local g_sea_transform_index = -1;
 
 local g_current_pos_x1 = 0;
 local g_current_pos_x2 = 0;
@@ -26,14 +29,20 @@ function Module.initialize()
     g_current_pos_y = Module.TargetWaveHeight;
 
     g_wave_back_mesh_index = new_mesh(Module.WaveMeshResourceIndex);
+    g_wave_back_transform_index = transform_create();
+    object_set_transform(g_wave_back_mesh_index, g_wave_back_transform_index);
     set_mesh_texture(g_wave_back_mesh_index, Module.Wave2TextureResourceIndex);
     move_transparent_mesh_to_back(g_wave_back_mesh_index);
 
-    g_wave_front_mesh_index =  new_mesh(Module.WaveMeshResourceIndex);
+    g_wave_front_mesh_index = new_mesh(Module.WaveMeshResourceIndex);
+    g_wave_front_transform_index = transform_create();
+    object_set_transform(g_wave_front_mesh_index, g_wave_front_transform_index);
     set_mesh_texture(g_wave_front_mesh_index, Module.Wave1TextureResourceIndex);
     move_transparent_mesh_to_front(g_wave_front_mesh_index);
 
     g_sea_mesh_index = new_mesh(Module.SeaMeshResourceIndex);
+    g_sea_transform_index = transform_create();
+    object_set_transform(g_sea_mesh_index, g_sea_transform_index);
     set_mesh_texture(g_sea_mesh_index, Module.SeaTextureResourceIndex);
     move_transparent_mesh_to_front(g_sea_mesh_index);
 
@@ -90,17 +99,14 @@ function Module.update()
     local iAdj1 = math.cos((g_wave_animation_cycle_degrees - 90) * math.pi / 180.0) * 512 / 300 - 13;
     local iAdj2 = math.cos(g_wave_animation_cycle_degrees * math.pi / 180.0) * 512 / 300 - 13;
 
-    set_identity_mesh_matrix(g_wave_front_mesh_index);
-    translate_mesh_matrix(g_wave_front_mesh_index, g_current_pos_x1 + iAdj1, g_current_pos_y + iHeight1 / 5, 0 - 0.1);
+    transform_set_translation(g_wave_front_transform_index, g_current_pos_x1 + iAdj1, g_current_pos_y + iHeight1 / 5, 0 - 0.1);
     set_mesh_is_visible(g_wave_front_mesh_index, true);
 
-    set_identity_mesh_matrix(g_sea_mesh_index);
-    scale_mesh_matrix(g_sea_mesh_index, 1, 1.5, 1);
-    translate_mesh_matrix(g_sea_mesh_index, g_current_pos_x1 + iAdj1, g_current_pos_y + iHeight1 / 5, 0 - 0.1);
+    transform_set_scale(g_sea_transform_index, 1, 1.5, 1);
+    transform_set_translation(g_sea_transform_index, g_current_pos_x1 + iAdj1, g_current_pos_y + iHeight1 / 5, 0 - 0.1);
     set_mesh_is_visible(g_sea_mesh_index, true);
 
-    set_identity_mesh_matrix(g_wave_back_mesh_index);
-    translate_mesh_matrix(g_wave_back_mesh_index, g_current_pos_x2 + iAdj2, g_current_pos_y + iHeight2 / 5, 6.5);
+    transform_set_translation(g_wave_back_transform_index, g_current_pos_x2 + iAdj2, g_current_pos_y + iHeight2 / 5, 6.5);
     set_mesh_is_visible(g_wave_back_mesh_index, true);
 
     if Module.GameLogic.is_player_colliding_with_rect(
