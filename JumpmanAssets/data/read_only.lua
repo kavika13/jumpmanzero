1a5b1f13edbe469c3efc7081458b86ad015686b1
Merlyn Morgan-Graham <kavika@gmail.com>
2021-05-23 20:35:29 -0700

    Add len metamethod to read_only tables (for `#table` support)

--- JumpmanAssets/data/read_only.lua
+++ JumpmanAssets/data/read_only.lua
@@ -6,6 +6,9 @@ function Module.make_table_read_only(tbl)
         __newindex = function(t, key, value)
             error("attempting to change constant " .. tostring(key) .. " to " .. tostring(value), 2);
         end,
+        __len = function(t)
+            return #tbl;
+        end,
         __pairs = function(t)
             return next, tbl, nil;
         end,
